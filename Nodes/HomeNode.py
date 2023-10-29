b23621551a4cee16227924c4c96c7024eef0f127
Rus4Lex <143345610+Rus4Lex@users.noreply.github.com>
2023-10-29 20:38:26 +0200

    create HomeNode

--- Nodes/HomeNode.py
+++ Nodes/HomeNode.py
@@ -1,15 +1,38 @@
-from Node import *
+from FloorNode import *
+
+
 
 class HomeNode(Node):
+    floor_max = 100
     def __init__(self):
         super().__init__(self)
         self.floor_count = 0
-        self.outrooms = []
+        self.outrooms = []#виселені жителі
         self.maxID = 0
 
     def get_maxID(self):
         self.maxID += 1
         return self.maxID
-    def add_floor(self):
+
+    def add_floor(self, rand=False):
+        if self.floor_count >= HomeNode.floor_max:
+            return False
+        tfn = FloorNode(self)
+        # додає поверх із випадковою кількістю квартир та кімнат в них
+        if rand:
+            for i in range(rint(1, HomeNode.floor_max)):
+                tfn.add_apartmet()
+        # додає поверх із одною квартирою з випадковою кількістю кімнат
+        else:
+            tfn.add_apartmet()
         self.floor_count += 1
-        self.__dict__[str(hex(self.floor_count))[2:]] = None
+        return True
+
+    def add_floor(self):
+        pass
+
+
+
+
+
+
