3a531c32b0fcf2c0d6e4060578e092110cb50f66
Rus4Lex <143345610+Rus4Lex@users.noreply.github.com>
2023-10-29 18:10:50 +0200

    createNodes

--- Nodes/ApartmentNode.py
+++ Nodes/ApartmentNode.py
@@ -1,10 +1,38 @@
-from Node import Node
+from InhabitantNode import *
 
 class ApartmentNode(Node):
     def __init__(self, id):
         super().__init__()
         self.id = id
-        self.rooms_count: int = 0
+        self.rooms_count: int = 1
+        self.inhabs_count: int = 0
 
-    def add_inhabitant(self, pib, age):
+
+    def add_rooms(self, cnt):
+        if self.rooms_count + cnt > 4:
+            return False
+        else:
+            self.rooms_count += cnt
+            return True
+
+    def sub_rooms(self, cnt):
+        if self.rooms_count - cnt < 1:
+            return False
+        else:
+            self.rooms_count -= cnt
+            return True
+
+
+    def add_inhabitant(self, inhab):
+        if self.rooms_count == self.inhabs_count:
+            return False
+        else:
+            tit = inhab
+            tit.parent = self
+            self.__dict__[hex(self.rooms_count)[2:]] = tit
+            self.inhabs_count += 1
+
+    def sub_inhabitants(self, ids:list):
         pass
+
+
