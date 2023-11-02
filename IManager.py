988a46c90e0e8807cdf96edb4c7e66b4a971934b
Rus4Lex <143345610+Rus4Lex@users.noreply.github.com>
2023-11-02 16:46:57 +0200

    IManager

--- IManager.py
+++ IManager.py
@@ -1,13 +1,16 @@
-from abc import abstractmethod
+from Nodes import *
 from Interface import Interface
 
 class IManager:
     def __init__(self):
-        ui_datbe = Interface()
-        ui_home = Interface()
-        ui_floor = Interface()
-        ui_apart = Interface()
-        ui_inhub = Interface()
-        ui_infos = Interface()
+        self.ui_datbe = Interface()#database interface
+        self.ui_home = Interface()#home interface
+        self.ui_floor = Interface()#floor interface
+        self.ui_apart = Interface()#apartament interface
+        self.ui_inhub = Interface()#inhubitant interface
+        self.ui_infos = Interface()#statistics interface
 
+        self.main_home = HomeNode()
+
+        # self.main_database = None
 
