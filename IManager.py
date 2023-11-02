0d7f4a47a18346e17f5d41c5d8521a81f189e4e5
Rus4Lex <143345610+Rus4Lex@users.noreply.github.com>
2023-11-02 16:46:57 +0200

    IManager

--- IManager.py
+++ IManager.py
@@ -1,5 +1,13 @@
 from abc import abstractmethod
-
+from Interface import Interface
 
 class IManager:
-    pass
+    def __init__(self):
+        ui_datbe = Interface()
+        ui_home = Interface()
+        ui_floor = Interface()
+        ui_apart = Interface()
+        ui_inhub = Interface()
+        ui_infos = Interface()
+
+
