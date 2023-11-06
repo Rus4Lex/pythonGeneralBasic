95d6cc0cd459ea02ff7c78e5af0cdac374f2adaa
Rus4Lex <143345610+Rus4Lex@users.noreply.github.com>
2023-11-06 22:51:26 +0200

    add test case for MDP file

--- MdpFile.py
+++ MdpFile.py
@@ -31,33 +31,3 @@ class MdpFile:
     @staticmethod
     def file_remove(name):
         os.remove(name)
-
-
-def random_word(length):
-    # letters = string.ascii_lowercase
-    letters = "".join([chr(a) for a in range(ord('а'), ord('я'))])
-    word = ''.join(choice(letters) for _ in range(length))
-    return word.capitalize()
-
-
-# Macro auto testing
-if __name__ == '__main__':
-    main_home = HomeNode()
-    for i in range(10):
-        main_home.add_floor(True)
-
-    floors = main_home.get_floors()
-
-    for i in floors:
-        aps = i.get_apartments()
-        for j in aps:
-            for k in range(randint(0, j.rooms_count)):
-                j.add_inhabitant(InhabitantNode(
-                    f"{random_word(randint(3, 12))} {random_word(randint(3, 12))} {random_word(randint(3, 15))}",
-                    randint(17, 65), main_home))
-                if randint(0, 10) > 9:
-                    ti = j.get_habitants()
-                    j.sub_inhabitants([ti[0]])
-
-    MdpFile.write(main_home, "testingHome.mdp")
-    mdp = MdpFile.read("testingHome.mdp")
---
+++ MdpFileTestCase.py
@@ -0,0 +1,52 @@
+import unittest
+from MdpFile import *
+
+class MdpFileTestCase(unittest.TestCase):
+
+    def random_word(self, length):
+        # letters = string.ascii_lowercase
+        letters = string.ascii_lowercase + "".join([chr(a) for a in range(ord('а'), ord('я'))])
+        word = ''.join(choice(letters) for _ in range(length))
+        return word.capitalize()
+    def test_empty_file_a(self):
+        try:
+            MdpFile.write(HomeNode(), "test.mdp")
+        except Exception as e:
+            self.fail(f"Raised writer!\n{e}")
+    def test_empty_file_b(self):
+        try:
+            MdpFile.read("test.mdp")
+        except Exception as e:
+            self.fail(f"Raised reader!\n{e}")
+
+    def test_big_data_file_a(self):
+        try:
+            main_home = HomeNode()
+            HomeNode.floor_max = 10000
+            for i in range(10000):
+                main_home.add_floor(True)
+
+            floors = main_home.get_floors()
+
+            for i in floors:
+                aps = i.get_apartments()
+                for j in aps:
+                    for k in range(randint(0, j.rooms_count)):
+                        j.add_inhabitant(InhabitantNode(
+                            f"{self.random_word(randint(1, 52))} {self.random_word(randint(1, 52))} {self.random_word(randint(1, 55))}",
+                            randint(1, 99), main_home))
+                        if randint(0, 10) > 9:
+                            ti = j.get_habitants()
+                            j.sub_inhabitants([ti[0]])
+            MdpFile.write(main_home, "bigFile.mdp")
+        except Exception as e:
+            self.fail(f"Raised writer big_data_file!\n{e}")
+    def test_big_data_file_b(self):
+        try:
+            MdpFile.read("bigFile.mdp")
+        except Exception as e:
+            self.fail(f"Raised reader!\n{e}")
+
+    def test_unknown_file_reading(self):
+        with self.assertRaises(Exception):
+            MdpFile.read("test2.mdp")
